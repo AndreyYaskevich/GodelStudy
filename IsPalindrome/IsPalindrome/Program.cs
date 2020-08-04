f679327ae57fc9bbb32e07767aee12feb8f9e39e
DESKTOP-MIT9Q8B\Yaskevich <andrez.yaskevich@gmail.com>
2020-08-04 18:53:54 +0300

    Solving IsPalyndrome

---
+++ IsPalindrome/IsPalindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IsPalindrome
+{
+    class Program
+    {
+        
+        static bool IsPalyndrome (string word)
+        {
+            char[] symbol = word.ToLower().ToCharArray();
+            
+            for (int i=0; i<symbol.Length/2; i++)
+            {
+                if (symbol[i] == symbol[symbol.Length - 1]) return true;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+
+            Console.WriteLine(IsPalyndrome(word));
+
+            Console.ReadKey();
+
+        }
+    }
+}

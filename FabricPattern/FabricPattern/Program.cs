672550c87598f6a35bb69eb2e945ab059b7a5973
DESKTOP-MIT9Q8B\Yaskevich <andrez.yaskevich@gmail.com>
2020-08-06 13:56:48 +0300

    Solving Factory Pattern

--- FabricPattern/FabricPattern/Program.cs
+++ FabricPattern/FabricPattern/Program.cs
@@ -4,13 +4,54 @@ namespace FabricPattern
 {
     class Program
     {
+        public class CarFactory {
+            public ICar MakeCar(string Name)
+            {
+                if (Name == "Tesla") return new ElectricCar();
+
+                return new EngineCar();
+            }
+        }
+
+        public interface ICar
+        {
+            void CreateCar();
+        }
+
+        public class EngineCar : ICar
+        {
+            public EngineCar()
+            {
+                CreateCar();
+            }
+            public void CreateCar()
+            {
+                Console.WriteLine("Created Car with internal combustion engine");
+            }   
+        }
+
+        public class ElectricCar : ICar
+        {
+            public ElectricCar()
+            {
+                CreateCar();
+            }
+
+            public void CreateCar()
+            {
+                Console.WriteLine("Create Car with electric engine");
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            CarManufacturer bmw = new EngineCarDeveloper("BMW");
-            Car engineCar = bmw.Create();
 
-            CarManufacturer tesla = new ElectricCarDeveloper("TESLA");
-            Car electricCAr = tesla.Create();
+            CarFactory factory = new CarFactory();
+
+            factory.MakeCar("Tesla");
+
+            factory.MakeCar("mercedes");
 
             Console.ReadKey();         
 

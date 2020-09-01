38492cd4443486c48553f8e4a9cf521adf984d95
DESKTOP-MIT9Q8B\Yaskevich <andrez.yaskevich@gmail.com>
2020-09-01 16:46:51 +0300

    Creating ASP.Net Core  Music Application

---
+++ MusicApplication/MusicApplication/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using MusicApplication.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApplication.Models
+{
+    public class Album : IDataObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SongId { get; set; }
+        public Song Song { get; set; }
+    }
+}
---
+++ MusicApplication/MusicApplication/Models/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApplication.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
